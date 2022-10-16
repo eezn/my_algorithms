402974e1338179d7b8bc7277493cb7b5e141fbf3
JIN-LEE <pplaneoo@gmail.com>
2022-10-16 15:53:44 +0900

    feat(sort.Utils): Add isSorted function

--- src/sort/basic/Utils.java
+++ src/sort/basic/Utils.java
@@ -6,4 +6,12 @@ public class Utils {
         data[a] = data[b];
         data[b] = temp;
     }
+
+    public static boolean isSorted(int[] data) {
+        for (int i = 0; i < data.length - 1; ++i) {
+            if (data[i] > data[i + 1])
+                return false;
+        }
+        return true;
+    }
 }
